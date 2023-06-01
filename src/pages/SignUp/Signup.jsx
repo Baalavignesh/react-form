e4f30801945d6a5c4eb7fada53354924e8160eff
Baalavignesh Arunachalam <baalavignesh21@gmail.com>
2023-06-01 11:05:57 +0530

    app skeleton

---
+++ src/pages/SignUp/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div className="flex w-full justify-center item-center h-screen ">
+      <div className="p-24 m-20 flex w-3/5 bg-slate-400 rounded-lg">
+        Signup Page
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
